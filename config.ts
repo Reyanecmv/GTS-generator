1473a687b24064427fbac329dd46a31705b69203
devcm <vlad.cristea@filed.com>
2021-03-21 00:41:35 +0200

    Working to save

--- config.ts
+++ config.ts
@@ -2,6 +2,6 @@ export const configuration = {
   rabbitConString: "amqps://digtmuzi:WOolK8L8EHqfQXmbCcsOALxMOUIoAt8a@cow.rmq2.cloudamqp.com/digtmuzi",
   rabbitQueue: "test1",
   rabbitExchange: "Test1",
-  alertNumber: 1000,
+  alertNumber: 50,
   alertInterval: 10
 }
